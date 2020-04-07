7801fbff33af1847710dddc8675e4b61aa3aa1ed
Vivers0 <vivers.savva@gmail.com>
2020-04-07 11:20:21 +0300

    added .env

--- index.js
+++ index.js
@@ -1,109 +1,120 @@
 require("@babel/core").transform("code");
-
+require("dotenv").config({ path: './src/.env' })
 // Parameters //
 
-import Discord from "discord.js";
+import { Client, Collection } from "discord.js";
 import mysql from 'mysql';
 import fs from 'fs';
 
-import genXP from './functions';
+import { genXP } from './src/functions';
+import config from './src/config';
 
-const client = new Discord.Client();
-const config = require("./config.json");
+const client = new Client();
+client.commands = new Collection();
 
+// Global Commands //
 global.prefix = config.prefix;
 
 
-client.commands = new Discord.Collection();
-client.login(config.token);
+
+// Login //
+client.login(process.env.TOKEN);
+
+
+
+// ________________________________________________________ //
+
+
 
 // Ready //
 
 client.on("ready", () => {
   setInterval(() => {
-    let index = Math.floor(Math.random() * (acivities_list.length - 1) + 1);
-    client.user.setActivity(acivities_list[index], { type: "WATCHING" });
+    let i = Math.floor(Math.random() * (config.Acivities_list.length - 1) + 1);
+    client.user.setActivity(config.Acivities_list[i],
+      { 
+        type: "WATCHING" 
+      });
   }, 10000);
-  // Console.log
-  console.log("Ok!");
-  client.user.setStatus("idle");
+
+  // Console log
+  console.log(`[ 👍 ] Бот ${client.user.username} запущен.`);
+  client.user.setStatus("online");
 });
 
 // Loader //
 fs.readdir("./lib", (err, files) => {
   if (err) console.log(err);
   var jsfiles = files.filter(f => f.split(".").pop() === "js");
-  if (jsfiles.length <= 0) console.log("Нет комманд для загрузки!!");
-  console.log(`Найдено ${jsfiles.length} комманд`);
+  if (jsfiles.length <= 0) console.log("[ ⚠️ ] Нет комманд для загрузки!!");
   jsfiles.forEach((f, i) => {
     var props = require(`./lib/${f}`);
-    console.log(`${i + 1}.${f} загружена!`);
     client.commands.set(props.help.name, props);
   });
+  console.log(`[ 🔋 ] Запущены все ${jsfiles.length} модулей без ошибок!`)
 });
 
 // MySQL //
 
 const conn = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'eugen',
-  password : '1234',
-  database : 'eugen'
+  host     : process.env.DB_HOST,
+  user     : process.env.DB_USER,
+  password : process.env.DB_PASS,
+  database : process.env.DB_DATABASE,
+  charset  : 'utf8mb4_bin'
 });
 
 conn.connect(err => {
-  if(err) {                                     // or restarting (takes a while sometimes).
-    console.log('error when connecting to db:', err);
-  }    
-  console.log('MySQL COnnect!');
+  if (err) console.log('[ ⚠️ ] Ошибка при запуске подключения к ДБ!')
+  else console.log('[ ☀️ ] MySQL Подключён!');
+});
+
+// Member Add //
+client.guilds.cache
+
+client.on("guildMemberAdd", async member => {
+  conn.query(`SELECT * FROM account WHERE d_id = ?`, [member.id], (error, rows, fields) => {
+    if (error) throw error;
+      if (rows.length < 1) {
+        conn.query(`INSERT INTO account (d_id, level, xp, money, descrip) VALUES (?,?,?,?,?)`, [member.id,0,0,0,'Введите описание, команда \`!drcr\`'], console.log);
+    } 
+  });
 });
 
 // Messages Loader //
 
 client.on("message", message => {
   if (message.author.bot) return;
-  if (message.channel.type === "dm") return;
+
 
   conn.query('SELECT * FROM account WHERE d_id = ?', [message.author.id], (err, rows) => {
-    if(err) throw err;
+    if (err) throw err;
     let curlvl = Number(rows[0].level);
     let nxtlvl = Number(rows[0].level * 1000);
-    let xp = Number(rows[0].xp);
-    let xpp = xp + Number(genXP());
-    if(rows.length >= 1){
+    let xp     = Number(rows[0].xp);
+    let xpp    = xp + Number(genXP());
+
+    if (rows.length >= 1) {
       conn.query(`UPDATE account SET xp = ? WHERE d_id = ?`, [xpp, message.author.id])
+    } else {
+      conn.query(`INSERT INTO account (d_id, level, xp, money, descrip) VALUES (?,?,?,?,?)`, [member.id,0,0,0, 'Введите описание, команда \`!drcr\`'], console.log);
     }
-    if(nxtlvl <= xp){
+
+    if (nxtlvl <= xp) {
       conn.query('UPDATE account SET level = ? WHERE d_id = ?', [curlvl+1, message.author.id])
     }
-  })
-  
-  let messageArray = message.content.split(" ");
- global.cmd = messageArray[0];
-  global.args = messageArray.slice(1);
-  let commandfile = client.commands.get(cmd.slice(prefix.length));
-  if (commandfile) {
-    commandfile.run(client, message, args, conn).catch(error => console.log(error.message))
-  }
+  });
+
+
+  const messageArray = message.content.split(" ");
+  const cmd          = messageArray[0];
+  const args         = messageArray.slice(1);
+
+  let commandfile  = client.commands.get(cmd.slice(prefix.length));
+  if (commandfile) commandfile.run(client, message, args, conn).catch(error => console.log(error.message));
 });
 
 
 // Guild Stats //
 
-client.on("guildMemberAdd", member => {
-
-    conn.query(`SELECT * FROM account WHERE d_id = ?`, [member.id], (error, rows, fields) => {
-      if(error) throw error;
-        if(rows.length < 1) {
-          conn.query(`INSERT INTO account (name, d_id, level, xp, money) VALUES (?,?,?,?,?)`, [member.user.tag,member.id,0,0,0], console.log);
-        } 
-      });
-      
-      conn.query("SELECT * FROM bans WHERE user_id = ?", [member.id], (error, rows, fields) => {
-        if (error) throw error;
-        if(rows.length > 0) {
-          member.guild.owner.send(new RichEmbed().setAuthor("Внимание!").setDescription("На ваш сервер зашел человек, который находится в бан-листе у нашего бота.").addBlankField().addField("Участник:", "<@" + rows[0].user_id + ">", true).addField("Причина:", rows[0].reason, true).setColor("RED"));
-        }
-      })
-});
 
--- src/functions.js
+++ src/functions.js
@@ -1,7 +1,30 @@
+import { RichEmbed } from "discord.js";
 
-function genXP(){
-    
-    return Math.floor(Math.random() * 10)+10;
-}
+export function genXP()
+{
+    return Math.floor(Math.random() * 10) + 10;
+};
 
-module.exports = genXP;
+export const noPermissions =
+ new RichEmbed()
+    .setColor("ORANGE")
+    .setDescription("У вас нет прав на выполнение этой операции!")
+    .setTimestamp();
+
+export const ErrorSql =
+    new RichEmbed()
+    .setColor("ORANGE")
+    .setDescription("Ошибка SQL базы! Обратитесь к администратору бота.")
+    .setTimestamp();
+
+export const noPeople =
+    new RichEmbed()
+        .setColor("ORANGE")
+        .setDescription("Вы не указали участника!")
+        .setTimestamp();
+
+export const noPermissionsMe = 
+    new RichEmbed()
+        .setColor("RED")
+        .setDescription("У меня нет прав!")
+        .setTimestamp();
